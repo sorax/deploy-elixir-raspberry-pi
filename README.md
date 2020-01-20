2d8954f1e9e13b0378cf8f9aee9664b61ce57b49
sorax <robert@hausgedacht.de>
2020-01-20 22:19:36 +0100

    add local setup script

--- README.md
+++ README.md
@@ -10,7 +10,12 @@
 
 2. Setup SD card
 
+- make sure the SD card is mounted (should be `/Volumes/boot`)
 - `chmod +x *.sh` (makes .sh files executable)
 - `./sd_setup.sh` (run script)
 
 3. Insert SD card into Raspberry Pi and power on
+
+4. Configure access & upload remote-script
+
+- `./pi_local.sh`
---
+++ pi_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo WLAN-Config
+read -p 'PI-HOST: ' HOST
+read -p 'PI-PORT: ' PORT
+
+echo "#############################"
+echo "# When asked for a password #"
+echo "# Type in: raspberry        #"
+echo "#############################"
+ssh-copy-id -p $PORT pi@$HOST
+ssh -t -p $PORT pi@$HOST sudo passwd -l pi
+
+scp -P $PORT hooks/post-receive pi@$HOST:/home/pi
+scp -P $PORT pi_remote.sh pi@$HOST:/home/pi
+ssh -t -p $PORT pi@$HOST chmod +x pi_remote.sh

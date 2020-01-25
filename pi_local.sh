d537d4f54d80dc07d1f9ab631908a6cf96a10a51
sorax <robert@hausgedacht.de>
2020-01-25 22:42:46 +0100

    disable hook-installs

--- pi_local.sh
+++ pi_local.sh
@@ -8,9 +8,9 @@ echo "# When asked for a password #"
 echo "# Type in: raspberry        #"
 echo "#############################"
 ssh-copy-id -p $PORT pi@$HOST
-ssh -t -p $PORT pi@$HOST sudo passwd -l pi
+ssh -T -p $PORT pi@$HOST sudo passwd -l pi
 
 scp -P $PORT hooks/post-receive pi@$HOST:/home/pi
 scp -P $PORT pi_remote.sh pi@$HOST:/home/pi
-ssh -t -p $PORT pi@$HOST chmod +x pi_remote.sh
-ssh -t -p $PORT pi@$HOST sudo apt install -y screen
+ssh -T -p $PORT pi@$HOST chmod +x pi_remote.sh
+ssh -T -p $PORT pi@$HOST sudo apt install -y screen

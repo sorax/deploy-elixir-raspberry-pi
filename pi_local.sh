5ee48746bb5ede758990ac2ed56825c3ef7ae563
sorax <robert@hausgedacht.de>
2020-02-03 23:55:25 +0100

    prepare full remote execurion

--- pi_local.sh
+++ pi_local.sh
@@ -8,6 +8,7 @@ echo "#############################"
 echo "# When asked for a password #"
 echo "# Type in: raspberry        #"
 echo "#############################"
+
 ssh-copy-id -p $PORT pi@$HOST
 ssh -T -p $PORT pi@$HOST sudo passwd -l pi
 
@@ -15,3 +16,8 @@ scp -P $PORT hooks/post-receive pi@$HOST:/home/pi
 scp -P $PORT pi_remote.sh pi@$HOST:/home/pi
 ssh -T -p $PORT pi@$HOST chmod +x pi_remote.sh
 ssh -T -p $PORT pi@$HOST sudo apt install -y screen
+
+# TODO // NOT READY YET
+# pi_remote.sh requires a config (Repository, Domain, E-Mail)
+# we have to provide
+#ssh -T -p $PORT pi@$HOST screen -dm ./pi_remote.sh

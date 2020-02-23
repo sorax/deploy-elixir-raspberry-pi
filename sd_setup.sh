0335c4248a49bf9a65cc995a091223f6b0771ce4
sorax <robert@hausgedacht.de>
2020-02-23 23:09:02 +0100

    add ethernet option

--- sd_setup.sh
+++ sd_setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-echo WLAN-Config
-read -p 'WLAN-SSID: ' WLANSSID
-read -p 'Password: ' WLANPASS
-
 VOLUME=/Volumes/boot
 
 touch $VOLUME/ssh
-touch $VOLUME/wpa_supplicant.conf
 
+read -p "Use WLAN or Ethernet? (W)LAN (E)thernet " -n 1
+if [[ $REPLY =~ ^[Ww]$ ]]; then
+echo ""
+echo "WLAN-Config"
+read -p "WLAN-SSID: " WLANSSID
+read -p "Password: " WLANPASS
+
+touch $VOLUME/wpa_supplicant.conf
 /bin/cat <<EOM >$VOLUME/wpa_supplicant.conf
 country=DE
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
@@ -19,3 +22,4 @@ network={
   key_mgmt=WPA-PSK
 }
 EOM
+fi

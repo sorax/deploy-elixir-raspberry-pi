8620f6286f8f767f9b2302964d8bd237c80358a7
sorax <robert@hausgedacht.de>
2020-02-23 23:26:26 +0100

    local invoke remote script

--- pi_local.sh
+++ pi_local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "PI-Connect"
-read -p 'PI-HOST: ' HOST
-read -p 'PI-PORT: ' PORT
+read -p "PI-HOST: " HOST
+read -p "PI-PORT: " PORT
 
 echo "#############################"
 echo "# When asked for a password #"
@@ -16,8 +16,4 @@ scp -P $PORT hooks/post-receive pi@$HOST:/home/pi
 scp -P $PORT pi_remote.sh pi@$HOST:/home/pi
 ssh -T -p $PORT pi@$HOST chmod +x pi_remote.sh
 ssh -T -p $PORT pi@$HOST sudo apt install -y screen
-
-# TODO // NOT READY YET
-# pi_remote.sh requires a config (Repository, Domain, E-Mail)
-# we have to provide
-#ssh -T -p $PORT pi@$HOST screen -dm ./pi_remote.sh
+ssh -T -p $PORT pi@$HOST screen -dmS Setup ./pi_remote.sh

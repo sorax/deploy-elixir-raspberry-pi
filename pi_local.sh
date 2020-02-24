94dbff9356662df140bc70fc94eb38a71dc1e95e
sorax <robert@hausgedacht.de>
2020-02-24 23:26:09 +0100

    create remote-config

--- pi_local.sh
+++ pi_local.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "PI-Connect"
 read -p "PI-HOST: " HOST
 read -p "PI-PORT: " PORT
-
+read -p "git repo name: " REPOSITORY
+read -p "use https? (Yn) " -n 1 HTTPS
+if [[ $HTTPS =~ ^[Nn]$ ]]; then
+  echo ""
+else
+  echo ""
+  read -p "Domain: " DOMAIN
+  read -p "E-Mail: " MAIL
+fi
 echo "#############################"
-echo "# When asked for a password #"
-echo "# Type in: raspberry        #"
+echo "When asked for a password"
+echo "Type in: raspberry"
 echo "#############################"
 
 ssh-copy-id -p $PORT pi@$HOST
@@ -15,5 +22,16 @@ ssh -T -p $PORT pi@$HOST sudo passwd -l pi
 scp -P $PORT hooks/post-receive pi@$HOST:/home/pi
 scp -P $PORT pi_remote.sh pi@$HOST:/home/pi
 ssh -T -p $PORT pi@$HOST chmod +x pi_remote.sh
+
+ssh -T -p $PORT pi@$HOST << EOSSH
+touch setup.cfg
+/bin/cat <<EOM >setup.cfg
+REPOSITORY=$REPOSITORY
+HTTPS=$HTTPS
+DOMAIN=$DOMAIN
+MAIL=$MAIL
+EOM
+EOSSH
+
 ssh -T -p $PORT pi@$HOST sudo apt install -y screen
 ssh -T -p $PORT pi@$HOST screen -dmS Setup ./pi_remote.sh

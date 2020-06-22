0af7758747bd8325f1b1969091046bdd1fcfe40d
sorax <info@sorax.net>
2020-06-22 17:47:20 +0200

    prevent globbing

--- pull.sh
+++ pull.sh
@@ -3,22 +3,22 @@
 . setup.cfg
 
 URL="https://github.com/$GIT_USER/$GIT_REPO/releases/latest"
-NEW_VERSION="$(curl -s $URL | grep -o 'releases/tag/.*"' | cut -d '"' -f 1 | cut -d / -f 3)"
+NEW_VERSION="$(curl -s "$URL" | grep -o 'releases/tag/.*"' | cut -d '"' -f 1 | cut -d / -f 3)"
 
 echo "Current Version: $CUR_VERSION => New Version: $NEW_VERSION"
 
 if [ "$NEW_VERSION" != "$CUR_VERSION" ]; then
   echo "Installing version $NEW_VERSION"
 
-  rm -Rf releases/$NEW_VERSION
-  mkdir -p releases/$NEW_VERSION
-  cd releases/$NEW_VERSION
+  rm -Rf releases/"$NEW_VERSION"
+  mkdir -p releases/"$NEW_VERSION"
+  cd releases/"$NEW_VERSION" || exit
 
   wget "https://github.com/$GIT_USER/$GIT_REPO/archive/$NEW_VERSION.tar.gz"
   ## wget "https://github.com/$GIT_USER/$GIT_REPO/releases/download/$NEW_VERSION/release.zip"
   wget "https://github.com/$GIT_USER/$GIT_REPO/releases/download/$NEW_VERSION/static.zip"
 
-  tar -xvf $NEW_VERSION.tar.gz --strip 1
+  tar -xvf "$NEW_VERSION".tar.gz --strip 1
   unzip static.zip
 
   export DATABASE_URL=ecto://postgres:postgres@localhost/$GIT_REPO
@@ -30,7 +30,7 @@ if [ "$NEW_VERSION" != "$CUR_VERSION" ]; then
 
   mix deps.get --only prod
 
-  if [ "$CUR_VERSION" == "NONE"]; then
+  if [ "$CUR_VERSION" == "NONE" ]; then
     sudo -u postgres psql -c "CREATE DATABASE $REPOSITORY;"
     mix ecto.setup
   else
@@ -40,7 +40,7 @@ if [ "$NEW_VERSION" != "$CUR_VERSION" ]; then
   mix phx.digest
   MIX_ENV=prod mix release
 
-  sudo releases/$CUR_VERSION/_build/prod/rel/$GIT_REPO/bin/$GIT_REPO stop
+  #sudo releases/"$CUR_VERSION"/_build/prod/rel/"$GIT_REPO"/bin/"$GIT_REPO" stop
 
   #sudo _build/prod/rel/$GIT_REPO/bin/$GIT_REPO start
   #sudo _build/prod/rel/$GIT_REPO/bin/$GIT_REPO daemon

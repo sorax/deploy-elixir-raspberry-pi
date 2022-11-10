885ff0c77ea38fda46551201310eda8c59ea4e56
sorax <robert@hausgedacht.de>
2022-11-10 16:28:36 +0100

    fix connect script

--- connect.sh
+++ connect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ ! -f ".env" ]; then
+CONFIG=config/.env
+
+if [ ! -f "$CONFIG" ]; then
   echo "Error: Config is missing. Please run pi_local.sh first."
 else
   echo "Load config"
-  source .env
+  source $CONFIG
   echo "Connect"
   ssh pi@$PI_HOST
 fi
